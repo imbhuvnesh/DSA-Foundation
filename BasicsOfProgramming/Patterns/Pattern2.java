70de62b31af9e2abf49fcb4a79e6e46d2aa15366
imbhuvnesh <vbhuvenesh05@gmail.com>
2020-08-31 00:11:26 +0530

    Revert "Sorted the content"
    This reverts commit 2325c4080687b075b0d11f8161be6edd6d76116a.
    
    Back to Sorted the content

---
+++ BasicsOfProgramming/Patterns/Pattern2.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+/**
+ Iss wale pattern me hume same triangle print krna h
+ parr ulta
+ */
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        scn.close();
+
+        for (int i = n; i >= 1; i--) {     //abki baar loop ulta chla dia
+            for (int j = 1; j <= i; j++) {  //andar ka kaam same
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ BasicsOfProgramming/Patterns/Pattern5.java
@@ -0,0 +1,45 @@
+package Patterns;
+
+import java.util.Scanner;
+
+//is question me diamond pattern print krna hai
+// n hmesha odd hoga
+/**
+                   *
+               *   *   *
+           *   *   *   *   *
+               *   *   *
+                   *
+ */
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        scn.close();
+
+        int spaces = n / 2;     //spaces  n/2 lela kyuki usme max n/2 spaces hi hote h
+        int stars = 1;         //star 1 se start kia 
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("\t");
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*\t");
+            }
+
+            if (i <= n / 2) { //isme yrr agr i jb n/2 se kam ya eql h
+                spaces--;       //toh space kam aur start bdhao
+                stars += 2;
+            }
+            else {          //else ulta
+                spaces++;
+                stars -= 2;
+            }
+            System.out.println();
+        }
+    }
+}
