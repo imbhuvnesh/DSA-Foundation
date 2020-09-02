8621c197a099b96132501cfd01e7eee10ffc1324
imbhuvnesh <vbhuvenesh05@gmail.com>
2020-09-02 23:29:58 +0530

    AnyBase Addtion done

---
+++ A_Basics_Of_Programming/C_FunctionandArrays/AnybaseSubtraction.java
@@ -0,0 +1,23 @@
+package C_FunctionandArrays;
+
+import java.util.Scanner;
+
+/*Iss question me 2 numbers "n1", "n2" given with same base "b"
+    Unka Subtraction krnaa hai
+    e.g. n1 = 1, n2 = 100, b = 8, result = 77
+    Given = n2 > n1
+    
+*/
+
+public class AnybaseSubtraction {
+    public static int anBaseSubtraction(int b, int n1. int n2) {
+        int result = 0;
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+    }
+}
