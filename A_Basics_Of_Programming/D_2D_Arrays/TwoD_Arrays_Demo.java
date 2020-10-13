97da8b558a58241038fc714941786e9993d5ed5c
imbhuvnesh <vbhuvenesh05@gmail.com>
2020-10-13 23:51:36 +0530

    D/2D arrays demo done

---
+++ A_Basics_Of_Programming/D_2D_Arrays/TwoD_Arrays_Demo.java
@@ -0,0 +1,30 @@
+package D_2D_Arrays;
+
+//just print an 2d Array
+
+import java.util.Scanner;
+
+public class TwoD_Arrays_Demo {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int r = scn.nextInt();
+        int c = scn.nextInt();
+
+        int[][] A = new int[r][c];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = scn.nextInt();
+            }
+        }
+
+        scn.close();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
